c02ef277282b5eb141d80adad0f2284486c28016
Vinícius Bressanini <vinimarcus41@gmail.com>
2022-05-18 14:40:29 -0300

    Adicionando domains (receita, despesa e conta)

--- src/main/java/com/api/controledespesas/domain/Despesa.java
+++ src/main/java/com/api/controledespesas/domain/Despesa.java
@@ -4,27 +4,30 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Entity
-public class Conta {
+public class Despesa {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @NotEmpty(message = "This attribute can´t be empty ")
-    private String nome;
+    private String tipo;
+
     @NotEmpty (message = "This attribute can´t be empty ")
-    private double saldo;
+    private String data;
+
     @NotEmpty (message = "This attribute can´t be empty ")
-    private String tipoConta;
+    private double valor;
+
+    @ManyToOne
     @NotEmpty (message = "This attribute can´t be empty ")
-    private String agencia;
+    private Conta conta;
 
 }
--- src/main/java/com/api/controledespesas/domain/Receita.java
+++ src/main/java/com/api/controledespesas/domain/Receita.java
@@ -4,27 +4,29 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Entity
-public class Conta {
+public class Receita {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotEmpty(message = "This attribute can´t be empty ")
-    private String nome;
+
     @NotEmpty (message = "This attribute can´t be empty ")
-    private double saldo;
+    private String tipo;
+
     @NotEmpty (message = "This attribute can´t be empty ")
-    private String tipoConta;
+    private String data;
+
     @NotEmpty (message = "This attribute can´t be empty ")
-    private String agencia;
+    private double valor;
+
+    @ManyToOne
+    @NotEmpty (message = "This attribute can´t be empty ")
+    private Conta conta;
 
 }
