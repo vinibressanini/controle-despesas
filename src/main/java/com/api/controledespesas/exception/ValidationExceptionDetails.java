f83252a984d5b92d2979fc4160a444df2ff767f2
Vinícius Bressanini <vinimarcus41@gmail.com>
2022-05-29 20:07:23 -0300

    Adicionando e configurando handler para Method Argument Not Valid Exception

---
+++ src/main/java/com/api/controledespesas/exception/ValidationExceptionDetails.java
@@ -0,0 +1,13 @@
+package com.api.controledespesas.exception;
+
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@SuperBuilder
+public class ValidationExceptionDetails extends ExceptionDetails{
+
+private final String fields;
+private final String fieldsMessage;
+
+}
--- src/main/java/com/api/controledespesas/handler/RestExceptionHandler.java
+++ src/main/java/com/api/controledespesas/handler/RestExceptionHandler.java
@@ -2,19 +2,26 @@ package com.api.controledespesas.handler;
 
 import com.api.controledespesas.exception.BadRequestException;
 import com.api.controledespesas.exception.BadRequestExceptionDetails;
+import com.api.controledespesas.exception.ValidationExceptionDetails;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<BadRequestExceptionDetails> handlerBadRequestException (BadRequestException bre) {
+    public ResponseEntity<BadRequestExceptionDetails> handleBadRequestException (BadRequestException bre) {
 
         return new ResponseEntity<>(
                 BadRequestExceptionDetails.builder()
@@ -26,5 +33,26 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
                         ,HttpStatus.BAD_REQUEST);
 
     }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception,
+                                                                  HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        List<FieldError>  fieldErrors = exception.getBindingResult().getFieldErrors();
+        String fields = fieldErrors.stream().map(FieldError::getField).collect(Collectors.joining(","));
+        String fieldsMessage = fieldErrors.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(","));
+
+        return new ResponseEntity<>(
+                ValidationExceptionDetails.builder()
+                        .title("Bad Request Exception, Invalid Fields")
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .details("Check the field(s) errors")
+                        .timestamp(LocalDateTime.now())
+                        .fieldsMessage(fieldsMessage)
+                        .fields(fields)
+                        .build(),HttpStatus.BAD_REQUEST);
+
+    }
+
 
 }
