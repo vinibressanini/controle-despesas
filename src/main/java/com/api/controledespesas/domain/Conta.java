bf7ea2b3bd67ee0ae03d6edb49ac5fc0bcba06c1
Vinícius Bressanini <vinimarcus41@gmail.com>
2022-05-29 20:02:37 -0300

    Consertando validação

--- src/main/java/com/api/controledespesas/domain/Conta.java
+++ src/main/java/com/api/controledespesas/domain/Conta.java
@@ -1,6 +1,7 @@
 package com.api.controledespesas.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,23 +9,20 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotEmpty;
 
+
+@Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
 @Entity
 public class Conta {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotEmpty(message = "This attribute can´t be empty ")
     private String nome;
-    @NotEmpty (message = "This attribute can´t be empty ")
     private double saldo;
-    @NotEmpty (message = "This attribute can´t be empty ")
     private String tipoConta;
-    @NotEmpty (message = "This attribute can´t be empty ")
     private String agencia;
 
 }
